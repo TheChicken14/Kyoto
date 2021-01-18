fa9b2a18133eace2d78b73ebc5f3b204c43b695c
Wisse <wissehes@gmail.com>
2021-01-18 16:51:57 +0100

    Add registry

--- src/Kyoto.ts
+++ src/Kyoto.ts
@@ -7,3 +7,12 @@ const client = new KyotoClient({
     owner: config.owners,
     invite: "https://discord.gg/4s4QUbQ"
 })
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['sound', 'Sounds']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'))
